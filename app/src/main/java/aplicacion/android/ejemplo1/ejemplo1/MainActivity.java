e1cdbc5c48371d7f69bb3c3b391ce7aae0ba0d12
Carmen Judith Cordero <carmen@MacBook-Air-de-Carmen.local>
2017-07-19 23:29:11 -0500

    prueba master

---
+++ app/src/main/java/aplicacion/android/ejemplo1/ejemplo1/MainActivity.java
@@ -0,0 +1,16 @@
+package aplicacion.android.ejemplo1.ejemplo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "hola mundo ejemplo", Toast.LENGTH_SHORT).show();
+    }
+}
